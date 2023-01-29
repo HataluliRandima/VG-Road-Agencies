621ea744c95fb37dcea579c6dbb852edbdccc707
Hataluli Randima <100038171+HataluliRandima@users.noreply.github.com>
2023-01-29 02:22:45 +0200

    added class of Citizen register

---
+++ VG ApiF/VG ApiF/DTO/CitizenRegister.cs
@@ -0,0 +1,23 @@
+﻿namespace VG_ApiF.DTO
+{
+    public class CitizenRegister
+    {
+      
+
+        public string CitiName { get; set; } = null!;
+
+        public string CitiLastname { get; set; } = null!;
+
+        public string CitiEmail { get; set; } = null!;
+
+        public string CitiPassword { get; set; } = null!;
+
+        public string CitiContactdetails { get; set; } = null!;
+
+        public string CitiAddress { get; set; } = null!;
+
+        public string CitiCity { get; set; } = null!;
+
+        public string CitiProvince { get; set; } = null!;
+    }
+}
