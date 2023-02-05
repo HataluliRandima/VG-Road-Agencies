98f7d8eb9295a87359cf82b8c162c24ede0ecd52
Hataluli Randima <100038171+HataluliRandima@users.noreply.github.com>
2023-02-05 16:10:06 +0200

    added report class and controller

---
+++ VG ApiF/VG ApiF/Controllers/ReportsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VG_ApiF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+    }
+}
---
+++ VG ApiF/VG ApiF/DTO/AddReport.cs
@@ -0,0 +1,45 @@
+﻿namespace VG_ApiF.DTO
+{
+    public class AddReport
+    {
+        //public int RId { get; set; }
+
+        public string RDescription { get; set; } = null!;
+
+        //public string? RPics1 { get; set; }
+
+        //public string? RPics2 { get; set; }
+
+        //public string? RPics3 { get; set; }
+
+        public string RType { get; set; } = null!;
+
+        //public string RStatus { get; set; } = null!;
+
+        public DateTime? RDatereported { get; set; }
+
+        public TimeSpan? RTimereported { get; set; }
+
+        //public TimeSpan? RViewtime { get; set; }
+
+        //public DateTime? RViewdate { get; set; }
+
+        //public DateTime? RDateupdated { get; set; }
+
+       // public string? RLocationpoint { get; set; }
+
+        public string RAddress { get; set; } = null!;
+
+        public string? RLat { get; set; }
+
+        public string? RLong { get; set; }
+
+        public string RCity { get; set; } = null!;
+
+        public string RProvince { get; set; } = null!;
+
+        //public int OfiId { get; set; }
+
+        //public int CitiId { get; set; }
+    }
+}
